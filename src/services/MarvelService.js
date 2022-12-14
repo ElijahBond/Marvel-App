305a01ae36aaf772094a170eb4ec957863261988
ElijahBond <bondarefil@gmail.com>
2022-12-14 23:00:03 +0300

    upd randomChar / add spinner and errorMessage

---
+++ src/services/MarvelService.js
@@ -0,0 +1,39 @@
+
+
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=510b8bce9fde45b9268c2a88929a9cf9';
+
+
+    getResoure = async (url) => {
+    let res = await fetch(url);
+
+    if (!res.ok) {
+        throw new Error(`Could not fetch ${url}, status ${res.status}`);
+    }
+
+    return await res.json();
+    }
+
+    getAllCharacters = async () => {
+        const res = this.getResoure(`${this._apiBase}characters?limit=9&offset=571&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
+
+    getCharacter = async (id) => {
+       const res = await this.getResoure(`${this._apiBase}characters/${id}?${this._apiKey}`);
+       return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 230)}...` : 'This character have not description',
+            thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url 
+        }
+    }
+}
+
+export default MarvelService;
